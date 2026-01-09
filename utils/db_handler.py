d139d6326ab7c1f65f5555f1a36c565d84b7894d
lxbe3 <vladesdol@gmail.com>
2026-01-09 17:07:09 +0300

    Fix ellipsis and auto-create table

--- utils/db_handler.py
+++ utils/db_handler.py
@@ -3,7 +3,7 @@ import os
 
 class DBHandler:
     def __init__(self):
-        # Используем os.getenv, чтобы приоритет был у настроек из CI
+        # Подключаемся, используя переменные окружения или значения по умолчанию
         self.connection = psycopg2.connect(
             dbname=os.getenv("DB_NAME", "test_api_qa"),
             user=os.getenv("DB_USER", "postgres"),
@@ -11,7 +11,6 @@ class DBHandler:
             host=os.getenv("DB_HOST", "host.docker.internal"),
             port=os.getenv("DB_PORT", "5432")
         )
-        self.connection = psycopg2.connect(...)
         self.cursor = self.connection.cursor()
         self.setup_db()
 
@@ -28,28 +27,12 @@ class DBHandler:
         self.cursor.execute(query)
         self.connection.commit()
 
-
-
-    def get_posts_count(self):
-        self.cursor.execute("SELECT COUNT(*) FROM posts;")
-        count = self.cursor.fetchone()[0]
-        return count
-
-    # ВОТ ЭТОТ МЕТОД: Проверь, чтобы он был ВНУТРИ класса!
-    def get_post_by_title(self, title):
-        """Метод ищет запись в базе по заголовку"""
-        query = "SELECT id, title, body FROM posts WHERE title = %s;"
-        self.cursor.execute(query, (title,))
-        return self.cursor.fetchone() 
-    
     def create_post(self, title, body, user_id):
-        """Автоматически создает пост в базе"""
         query = "INSERT INTO posts (title, body, user_id) VALUES (%s, %s, %s);"
         self.cursor.execute(query, (title, body, user_id))
-        self.connection.commit() # ВАЖНО: коммит сохраняет изменения в базе!
+        self.connection.commit()
 
     def delete_post_by_title(self, title):
-        """Удаляет пост, чтобы не забивать базу"""
         query = "DELETE FROM posts WHERE title = %s;"
         self.cursor.execute(query, (title,))
         self.connection.commit()
